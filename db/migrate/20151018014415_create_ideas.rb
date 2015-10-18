82de1b8d19dc239fa72a83141b1257d5551973e7
erikvdw <ev176@georgetown.edu>
2015-10-18 01:45:31 +0000

    Generate idea model with title, body, and submissions references

---
+++ db/migrate/20151018014415_create_ideas.rb
@@ -0,0 +1,12 @@
+class CreateIdeas < ActiveRecord::Migration
+  def change
+    create_table :ideas do |t|
+      t.string :title
+      t.text :body
+      t.belongs_to :submission, index: true
+
+      t.timestamps null: false
+    end
+    add_foreign_key :ideas, :submissions
+  end
+end
